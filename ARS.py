375508899522ead75df1a0c89b93343e74acb8e1
Teckeon <teckeon@outlook.com>
2018-08-05 21:58:57 -0500

    Policy evalutate function added

---
+++ ARS.py
@@ -0,0 +1,67 @@
+#ARS
+
+# Importing the libraries
+import os
+import numpy as np
+
+# Setting the Hyper Parameters
+
+class Hp():
+  
+  def __init__(self):
+    self.nb_steps = 1000
+    self.episode_length = 1000
+    self.learning_rate = 0.02
+    self.nb_directions = 16
+    self.nb_best_directions = 16
+    assert self.nb_best_directions <= self.nb_directions
+    self.noise = 0.03
+    self.seed = 1
+    self.env_name = ''
+
+# Normalizing the states
+    
+class Normalizer():
+  def __init__(self, nb_inputs):
+    self.n = np.zeros(nb_inputs)
+    self.mean = np.zeros(nb_inputs)
+    self.mean_diff = np.zeros(nb_inputs)
+    self.var = np.zeros(nb_inputs)
+    
+    
+  def observe(self, x):
+    self.n += 1.0
+    last_mean = self.mean.copy()
+    self.mean += (x - self.mean) / self.n
+    self.mean_diff += (x - last_mean) * (x - self.mean)
+    self.var = (self.mean_diff / self.n).clip(min = le-2) #makes sure var via clip is not equal to zero
+  
+  def normalize(self, inputs):
+    obs_mean = self.mean
+    obs_std = np.sqrt(self.var)
+    return (inputs - obs_mean) / obs_std
+  
+# Building the AI 
+    
+class Policy():
+  
+  def __init__(self, input_size, output_size):
+    self.theta = np.zeros((output_size, input_size))
+    
+  def evaluate(self, input, delta = None, direction = None):
+    if direction is None: 
+      return self.theta.dot(input)
+    elif direction == "positive":
+      return (self.theta + hp.noise*delta).dot(input)
+    else:
+      return (self.theta - hp.noise*delta).dot(input)
+    
+    
+      
+    
+    
+    
+    
+    
+    
+    
